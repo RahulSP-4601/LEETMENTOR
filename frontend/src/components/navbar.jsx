65edae0f20d232ce6acea9554405b3c2b2f1ea7c
RahulSP-4601 <rahulsp4601@gmail.com>
2025-07-04 15:16:29 -0500

    Navbar Design

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import './../css/navbar.css'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-content">
+        <div className="nav-logo">LEETMENTOR</div>
+        <div className="nav-links">
+          <Link to="/get-started">Get Started</Link>
+          {/* Add more options later as needed */}
+        </div>
+      </div>
+    </nav>
+  )
+}
