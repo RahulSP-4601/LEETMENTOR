2966f0718959342741e98da5ceffa876ab85e048
RahulSP-4601 <rahulsp4601@gmail.com>
2025-07-06 15:45:10 -0500

    Solved CORS Error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, origins=["http://localhost:5173"])  # allow frontend
+    CORS(app, origins=["http://localhost:5173"])  
     db.init_app(app)
     app.register_blueprint(auth_bp, url_prefix='/api')
 
